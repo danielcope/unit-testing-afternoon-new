0ca7e1ac71bdc5d17fb3504888b87de1b9f74faa
Daniel Cope <danielcope@users.noreply.github.com>
2021-04-01 07:03:33 -0700

    finished

--- src/__tests__/utilFunctions.test.js
+++ src/__tests__/utilFunctions.test.js
@@ -11,4 +11,24 @@ test('this should shorten text over 100 characters and add three periods at the 
 test('this should shorten text over 100 characters and add three periods at the end', ()=> {
   const shortenedText = shortenText(longText);
   
+  expect(shortenedText).not.toHaveLength(longText.length)
+  expect(shortenedText.slice(-3)).toBe('...')
+});
+
+test('the wordCount function should count the number of words in a sentence', () => {
+  expect(wordCount(posts)).toBe(233);
+});
+
+test('the function attachUserName should attach a users name to a post', () => {
+  const newPosts = attachUserName(users, posts);
+
+  expect(newPosts[0]).toHaveProperty('displayName');
 })
+
+test('attachUserName should remove posts that have no matching user', ()=> {
+  const newPosts = attachUserName(users,posts);
+  const deletedPost = posts[5];
+
+  expect(newPosts).not.toContainEqual(deletedPost);
+});
+
